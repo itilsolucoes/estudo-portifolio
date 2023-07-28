491225673dcc487515cc5f3b88c6f7143ba0b7d3
root <root@WELL-PC>
2023-07-28 12:53:36 -0300

    segund commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import "./App.css"
 
 //Quando o componente está exportado por default não se coloca a chave na importação, porém quando o mesmo não é exportado por padrão, se obriga a utilizar as chaves
 import { Hero } from "./components/Hero"
@@ -9,7 +10,7 @@ import { Service } from "./components/Service"
 import { Skills } from "./components/Skills"
 import { Testimonials } from "./components/Testimonials"
 
-import "./App.css"
+
 
 
 
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,9 +1,13 @@
 //Sessão Banner
 export function Hero() { // Exporte com chaves
-    return (
-      <section className="bg-gradient-to-tr from-black to to-gray-900">
-        <h2>Hero test</h2>
-      </section>
-    );
-  }
-  
+  return (
+    <section className="bg-gradient-to-tr from-black to-gray-900 "  style={{ color: "#ffffff" }}>
+
+      <div className="container mx-auto max-w-4xl">
+        <h1>Olá, me chamo Wellington</h1>
+      </div>
+    </section>
+  )
+}
+
+
